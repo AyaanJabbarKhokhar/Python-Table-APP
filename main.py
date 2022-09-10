bd0d8b9a10857996f65b0abad5530b4280ca4f28
-ᴀʏᴀᴀɴ ᴋʜᴏᴋʜᴀʀ <89092388+AyaanJabbarKhokhar@users.noreply.github.com>
2022-09-10 16:07:11 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# import random
+
+
+# print("Welcome to rock paper scissor game.")
+
+
+
+# my_list = ["rock","paper","scissor"]
+# random_choice = random.choice(my_list)
+# human = input("choose rock paper or scissor ")
+
+
+# print(f"Computer chose {random_choice}")
+# print(f"You chose {human}")
+
+# if human == random_choice:
+#   print("Draw")
+
+
+# if human == "rock" and random_choice == "scissor" :
+#   print("human win")
+# elif human == "scissor" and random_choice == "rock":
+#   print("You Lost :(")
+
+
+# if human == "scissor" and random_choice == "paper" :
+#   print("human win")
+# elif human == "paper" and random_choice == "scissor":
+#   print("You Lost :(")
+
+  
+# if human == "paper" and random_choice == "rock" :
+#   print("human win")
+# elif human == "rock" and random_choice == "paper":
+#   print("You Lost :(")
+
+
